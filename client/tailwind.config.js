64f2eab96c17eb2dff99158232bdb2055c8188be
coder-noman <abdhullaalnoman25@gmail.com>
2024-07-02 21:19:38 +0600

    Header Section completed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+    // 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+    // require('flowbite/plugin'),
+    // require('tailwind-scrollbar'),
+  ],
 }
 
