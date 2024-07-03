74734914c632a8a31420d4f856dd9951c0e03a97
coder-noman <abdhullaalnoman25@gmail.com>
2024-07-03 13:20:26 +0600

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-// import userRoutes from './routes/user.route.js';
-// import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 // import postRoutes from './routes/post.route.js';
 // import commentRoutes from './routes/comment.route.js';
 // import cookieParser from 'cookie-parser';
@@ -23,15 +23,15 @@ mongoose
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 // app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-// app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 // app.use('/api/post', postRoutes);
 // app.use('/api/comment', commentRoutes);
 
